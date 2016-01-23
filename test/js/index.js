7e8259e2ea53f053d0dffc3818e56500695c67c6
thirtysixwind <zxy_jndx@qq.com>
2016-01-23 10:24:53 +0800

    Create index.js

---
+++ test/js/index.js
@@ -0,0 +1,7 @@
+$('.nav .item').dropdown({
+	content : [['<a href="javascript:">内容1-1</a>','<a href="javascript:">内容1-2</a>','内容1-3'],
+			   ['内容2-1','内容2-1']],
+	'line-height' : 3,
+	'background-color' : 'rgba(255, 220, 220, .9)',
+	'border-color' : 'rgba(255, 220, 220, .9)'
+});
